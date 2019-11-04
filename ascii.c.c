70fcca999a2a27ce7bd3b0d0ca73d422b0b676bc
MUHAMMED YAZEEN AN <muhammedyaseen1012@gmail.com>
2019-11-04 23:30:39 +0530

    Add files via upload

---
+++ ascii.c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("@credits:messenger_1012 \nEnter any character:
+ ");
+
+    /* Reads the entered character and stores it
+     * into the char variable ch
+     */
+    scanf("%c", &ch);
+
+    /* Using the format specifiers we can get the ASCII code
+     * of a character. When we use %d format specifier for a
+     * char variable then it displays the ASCII value of a char
+     */
+    printf("ASCII value of character %c is: %d", ch, ch);
+    return 0;
+}
